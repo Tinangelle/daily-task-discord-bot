ce80a6e1430146aa90dd722fe1fac59341cfa80f
33 <angelxql0206@gmail.com>
2025-07-17 13:34:34 +0800

    Update main.py
    update emoji

--- main.py
+++ main.py
@@ -23,18 +23,18 @@ def get_tasks():
     
     # 添加每日任务
     msg += "🎯 每日任务：\n"
-    msg += "\n".join([f"① {row['任务内容']}" for row in rows 
+    msg += "\n".join([f"🔸 {row['任务内容']}" for row in rows 
                      if row["类型"] == "每日" and row["是否启用"] == "是"]) + "\n\n"
     
     # 添加限时任务
     msg += "⏰ 限时任务：\n"
-    msg += "\n".join([f"② {row['任务内容']} (截止 {row['截止日期']})" for row in rows 
+    msg += "\n".join([f"🕑 {row['任务内容']}（截止 {row['截止日期']}）" for row in rows 
                      if row["类型"] == "限时" and row["是否启用"] == "是" 
                      and row["截止日期"] and today.strftime('%Y-%m-%d') <= row["截止日期"]]) + "\n\n"
     
     # 添加长期任务
     msg += "♾️ 长期任务：\n"
-    msg += "\n".join([f"③ {row['任务内容']}" for row in rows 
+    msg += "\n".join([f"📜 {row['任务内容']}" for row in rows 
                      if row["类型"] == "长期" and row["是否启用"] == "是"])
     
     return msg
